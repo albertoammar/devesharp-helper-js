9fad77824531ed0d70010cb8e7b42f392916686c
Alberto Ammar <albertoammarw@gmail.com>
2020-01-10 21:34:59 -0300

    add function Arr compareArray

--- src/Arr.spec.ts
+++ src/Arr.spec.ts
@@ -6,4 +6,14 @@ describe('Arr', () => {
         expect([1,2,3,4]).toEqual(Arr.range(1,4));
         expect([1]).toEqual(Arr.range(1,1));
     });
+    
+    it('compareArray', async () => {
+        expect(Arr.compareArray([0],[0])).toBeTruthy();
+        expect(Arr.compareArray([0,1,3],[0,1,3])).toBeTruthy();
+        expect(Arr.compareArray([undefined,'b',3],[undefined,'b',3])).toBeTruthy();
+        expect(Arr.compareArray([0],[1])).toBeFalsy();
+        expect(Arr.compareArray([0],[0,1])).toBeFalsy();
+        expect(Arr.compareArray(['b'],['b','b'])).toBeFalsy();
+        expect(Arr.compareArray(['b'],['ba'])).toBeFalsy();
+    });
 });
